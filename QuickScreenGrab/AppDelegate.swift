ed8180d03b2bf16e8c903f1c72273944894db504
Connor Brewster <brewsterc@my.caspercollege.edu>
2015-08-11 14:08:45 -0600

    Initial Commit

---
+++ QuickScreenGrab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickScreenGrab
+//
+//  Created by connor on 8/11/15.
+//  Copyright © 2015 Double Bit Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuickScreenGrabTests/QuickScreenGrabTests.swift
@@ -0,0 +1,35 @@
+//
+//  QuickScreenGrabTests.swift
+//  QuickScreenGrabTests
+//
+//  Created by connor on 8/11/15.
+//  Copyright © 2015 Double Bit Studios. All rights reserved.
+//
+
+import XCTest
+
+class QuickScreenGrabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
